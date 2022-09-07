db572409412e38e0a04adad7079d73275f524ae9
Jensen <jj9876@live.com.au>
2022-09-07 22:07:10 +1000

    PWA stuff

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  pwa: {
+    themeColor: "#181818",
+    msTileColor: "#181818",
+    name: "BetterBetterCompass - VideoGamesClub",
+  },
   transpileDependencies: true,
 });
